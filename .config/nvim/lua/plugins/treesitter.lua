4db9f51326baf4a5585b6f02204670c695e8bde7
Ezechukwu Ojukwu <ezechukwu69@gmail.com>
2024-07-15 05:09:48 +0100

    Save dotfiles for neovim

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -92,7 +92,7 @@ return {
                     },
                 },
                 highlight = {
-                    enable = false,
+                    enable = true,
 
                     -- NOTE: these are the names of the parsers and not the filetype. (for example if you want to
                     -- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
