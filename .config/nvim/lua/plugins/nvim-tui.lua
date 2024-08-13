38656bf91d21d149ba6a79596f41af67c0fd075e
Ezechukwu Ojukwu <ezechukwu69@gmail.com>
2024-08-13 20:13:05 +0100

    Update TUI height margin

--- .config/nvim/lua/plugins/nvim-tui.lua
+++ .config/nvim/lua/plugins/nvim-tui.lua
@@ -4,14 +4,14 @@ return {
         require("tui").setup({
             name = "LazyGitTUI",
             command = "lazygit",
-            height_margin = 5,
+            height_margin = 2,
             width_margin = 5
         })
 
         require("tui").setup({
             name = "LazyYazi",
             command = "yazi",
-            height_margin = 5,
+            height_margin = 2,
             width_margin = 5
         })
     end,
